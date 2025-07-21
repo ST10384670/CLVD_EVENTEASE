5e2ac48f80b1aa9025ed2478f75707cd750a8fc9
zanenhlanhla <st10384670@vcconnect.edu.za>
2025-07-21 20:48:20 +0200

    Initial Commit

---
+++ EVENT_EASE/Migrations/20250721144721_allTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EVENT_EASE.Migrations
+{
+    /// <inheritdoc />
+    public partial class allTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
